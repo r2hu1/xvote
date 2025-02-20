7d77c1b904d35a01a8dfc525c2f6eb6c3cf66acb
r2hu1 <hi@rahul.eu.org>
2025-02-20 22:47:43 +0530

    thinking about  ui

---
+++ components/header.jsx
@@ -0,0 +1,15 @@
+import { Button } from "./ui/button";
+
+export default function Header() {
+    return (
+        <header className="flex items-center justify-between border-b-2 border-b-black px-6 md:px-20 lg:px-32 py-3">
+            <div className="flex items-center gap-2">
+                <h1 className="font-bold text-lg bg-black text-white w-8 h-8 flex items-center justify-center">X</h1>
+                <h1 className="text-lg font-bold">Vote</h1>
+            </div>
+            <div>
+                <Button variant="primary">Create</Button>
+            </div>
+        </header>
+    )
+};
