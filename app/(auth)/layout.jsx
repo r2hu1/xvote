8ff659374f140b0b0e0ece33c338d1bd6beee3c5
r2hu1 <hi@rahul.eu.org>
2025-02-21 15:32:43 +0530

    lwt

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,6 +1,6 @@
 export default function RootLayout({ children }) {
     return (
-        <main className="flex items-center justify-center px-6 h-full w-full left-0 right-0 absolute">
+        <main className="flex items-center justify-center px-6 h-full w-full py-20">
             {children}
         </main>
     )
