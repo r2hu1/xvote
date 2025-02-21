f7c39080aaee8c2ae874acab3801ad6149115587
r2hu1 <hi@rahul.eu.org>
2025-02-21 13:17:38 +0530

    chngd style

--- components/header.jsx
+++ components/header.jsx
@@ -1,14 +1,14 @@
+import { LogOut, Plus } from "lucide-react";
 import { Button } from "./ui/button";
+import Logo from "./logo";
 
 export default function Header() {
     return (
         <header className="flex items-center justify-between px-6 md:px-20 lg:px-32 py-5">
+            <Logo />
             <div className="flex items-center gap-2">
-                <h1 className="font-bold text-lg bg-black rounded text-white w-8 h-8 flex items-center justify-center">X</h1>
-                <h1 className="text-lg font-bold">Vote</h1>
-            </div>
-            <div>
-                <Button variant="primary">Create</Button>
+                <Button className="rounded h-9 px-4">Create</Button>
+                <Button className="rounded h-9 w-9" size="icon" variant="outline"><LogOut className="h-3 w-3" /></Button>
             </div>
         </header>
     )
