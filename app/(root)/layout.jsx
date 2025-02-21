43bf839fccaf18360517a68c16e1f7f9a007ba16
r2hu1 <hi@rahul.eu.org>
2025-02-21 20:09:51 +0530

    simple menu

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -1,10 +1,13 @@
+import CheckAuth from "@/components/checkAuth";
 import Header from "@/components/header";
+import Menu from "@/components/menu";
 
 export default function RootLayout({ children }) {
     return (
         <main>
             <Header />
             {children}
+            <CheckAuth ifA={<Menu />}></CheckAuth>
         </main>
     )
 }
---
+++ components/menu.jsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { Home, Loader2, RefreshCcw, Search, User } from "lucide-react"
+import { Button } from "./ui/button"
+import Link from "next/link"
+
+export default function Menu() {
+    return (
+        <div className="fixed z-10 bottom-4 left-4 right-4 flex items-center justify-center">
+            <div className="bg-secondary/50 overflow-hidden border border-border rounded w-fit flex">
+                <Button className="rounded-none" variant="ghost" asChild>
+                    <Link href="/">
+                        <Home className="h-4 w-4" />
+                    </Link>
+                </Button>
+                <Button className="rounded-none" variant="ghost" asChild>
+                    <Link href="/">
+                        <Search className="h-4 w-4" />
+                    </Link>
+                </Button>
+                <Button className="rounded-none" variant="ghost" asChild>
+                    <Link href="/">
+                        <User className="h-4 w-4" />
+                    </Link>
+                </Button>
+                <Button className="rounded-none" variant="ghost" asChild>
+                    <Link href="/">
+                        <RefreshCcw className="h-4 w-4" />
+                    </Link>
+                </Button>
+            </div>
+        </div>
+    )
+}
