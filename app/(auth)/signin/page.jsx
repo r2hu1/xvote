b1ff2834f10bcaa04e26d55b54356ed6e2aa5b90
r2hu1 <hi@rahul.eu.org>
2025-02-21 19:41:52 +0530

    minimal hash ui

--- app/(auth)/signin/page.jsx
+++ app/(auth)/signin/page.jsx
@@ -12,7 +12,7 @@ import {
 import { cn } from "@/lib/utils";
 import { getUser } from "@/server/getUser";
 import { signinUser } from "@/server/signinUser";
-import { ChevronLeft, Loader2 } from "lucide-react";
+import { ChevronLeft, FileJson, Loader2 } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -23,6 +23,7 @@ export default function Page() {
     const [hash, setHash] = useState("");
     const [selectedFile, setSelectedFile] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [loading2, setLoading2] = useState(false);
     const router = useRouter();
 
     const handleFileSelect = (e) => {
@@ -30,18 +31,29 @@ export default function Page() {
         const file = e.target.files[0];
         const reader = new FileReader();
         reader.readAsText(file);
-        reader.onload = (e) => {
+        reader.onload = async (e) => {
             try {
+                setLoading2(true);
                 const data = JSON.parse(e.target.result);
-                setUsername(data.username);
-                setHash(data.hash);
+                let username = data.username;
+                let hash = data.hash;
                 setSelectedFile({ name: file.name, isValid: data.username && data.hash });
+                let res = await signinUser({ username, hash });
+                let resData = JSON.parse(res);
+                if (resData.success) {
+                    toast.success("Signed in successfully!");
+                    router.push("/");
+                }
+                else {
+                    toast.error(resData.error);
+                }
             }
             catch (err) {
                 toast.error("Invalid hash file!");
                 setSelectedFile(null);
                 e.target.value = '';
             }
+            setLoading2(false);
         }
     };
 
@@ -93,34 +105,32 @@ export default function Page() {
                     </Button>
                 </div>
                 <h1 className="text-xl font-medium mt-2">Welcome Back</h1>
-                <p className="text-base max-w-xs text-foreground/80 -mt-2.5">Signin to your anonymous account to continue.</p>
+                <p className="text-base max-w-xs text-foreground/80 -mt-2.5">Signin to your anonymous account to continue.
+                    Don't have an account? <Link className="text-foreground underline" href="/signup">SignUp</Link></p>
             </div>
             <div className="mt-5">
                 <form onSubmit={handleSignin} className="grid gap-2">
-                    <span className="text-sm font-normal text-foreground/80">Contiue with hash file</span>
-                    <Label htmlFor="hashFile" className="mb-5 border-dashed rounded h-14 text-center flex items-center justify-center border-primary border">
-                        <p className="text-sm font-normal text-foreground/80">{selectedFile?.name ? selectedFile?.name : "Click to select"}</p>
-                    </Label>
-                    <Input multiple={false} onChange={handleFileSelect} className="hidden" type="file" accept="json" id="hashFile" name="hashFile" />
-                    {!selectedFile && (
-                        <>
-                            <Label htmlFor="username">Username</Label>
-                            <Input onChange={(e) => { setUsername(e.target.value) }} value={username} onFocus={(e) => { e.target.placeholder = "" }} onBlur={(e) => { e.target.placeholder = "Anonymous-user-1" }} autoComplete="off" id="username" name="username" placeholder="Anonymous-user-1" />
-                            <Label htmlFor="hash" className="mt-2">Hash</Label>
-                            <Input type="password" onChange={(e) => { setHash(e.target.value) }} value={hash} onFocus={(e) => { e.target.placeholder = "" }} onBlur={(e) => { e.target.placeholder = "**********" }} autoComplete="off" id="hash" name="hash" placeholder="**********" />
-                            <div>
-                                <Popover>
-                                    <PopoverTrigger>
-                                        <p className="text-sm text-foreground/80 hover:underline">Forgot your account details?</p>
-                                    </PopoverTrigger>
-                                    <PopoverContent className="text-sm -mr-20">Its encrypted we can't help you with that. Maybe create a new account?!</PopoverContent>
-                                </Popover>
-                            </div>
-                        </>
-                    )}
-                    <Button type="submit" className={cn("mt-2", selectedFile && "-mt-4")} disabled={loading}>{loading ? <Loader2 className="h-4 w-4 animate-spin" /> : "Continue"}</Button>
-                    <p className="text-sm text-foreground/80 text-center">Don't have an account? <Link className="text-foreground underline" href="/signup">SignUp</Link></p>
+                    <Label htmlFor="username">Username</Label>
+                    <Input onChange={(e) => { setUsername(e.target.value) }} value={username} onFocus={(e) => { e.target.placeholder = "" }} onBlur={(e) => { e.target.placeholder = "Anonymous-user-1" }} autoComplete="off" id="username" name="username" placeholder="Anonymous-user-1" />
+                    <Label htmlFor="hash" className="mt-2">Hash</Label>
+                    <Input type="password" onChange={(e) => { setHash(e.target.value) }} value={hash} onFocus={(e) => { e.target.placeholder = "" }} onBlur={(e) => { e.target.placeholder = "**********" }} autoComplete="off" id="hash" name="hash" placeholder="**********" />
+                    <div>
+                        <Popover>
+                            <PopoverTrigger>
+                                <p className="text-sm text-foreground/80 hover:underline">Forgot your account details?</p>
+                            </PopoverTrigger>
+                            <PopoverContent className="text-sm -mr-20">Its encrypted we can't help you with that. Maybe create a new account?!</PopoverContent>
+                        </Popover>
+                    </div>
+                    <Button type="submit" className="mt-2" disabled={loading}>{loading ? <Loader2 className="h-4 w-4 animate-spin" /> : "Continue"}</Button>
                 </form>
+                <div className="mt-5 grid gap-4">
+                    <p className="text-sm text-center text-foreground/80">Or</p>
+                    <Button asChild variant={loading2 ? "outline" : "secondary"} disabled={loading2}>
+                        <Label htmlFor="hashFile" className="flex items-center justify-center gap-2">{loading2 ? <Loader2 className="h-4 w-4 animate-spin" /> : <FileJson className="h-4 w-4" />} {loading2 ? "" : "Continue with hash file"}</Label>
+                    </Button>
+                    <Input multiple={false} onChange={handleFileSelect} className="hidden" type="file" accept=".json" id="hashFile" name="hashFile" />
+                </div>
             </div>
         </main>
     )
