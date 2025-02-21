39d180cab495590533ece93813c21020c66784be
r2hu1 <hi@rahul.eu.org>
2025-02-21 15:47:47 +0530

    utl fn

---
+++ components/checkAuth.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { getUser } from "@/server/getUser";
+import { useEffect, useState } from "react";
+
+export default function CheckAuth({ ifA, children }) {
+    const [authenticated, setAuthenticated] = useState(null);
+    useEffect(() => {
+        const fetchUser = async () => {
+            const user = await getUser();
+            if (user) {
+                setAuthenticated(true);
+            }
+        };
+        fetchUser();
+    }, []);
+    if (authenticated) return ifA;
+    return children;
+}
