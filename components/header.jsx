c7def6a6af1f506e3242c77281d32e79c185f717
r2hu1 <hi@rahul.eu.org>
2025-02-21 20:27:18 +0530

    rm sr and con

--- components/header.jsx
+++ components/header.jsx
@@ -2,11 +2,14 @@
 import { LogOut, Plus } from "lucide-react";
 import { Button } from "./ui/button";
 import Logo from "./logo";
-import { signOut } from "@/server/signOut";
-import CheckAuth from "./checkAuth";
 import Link from "next/link";
+import { useContext } from "react";
+import { AuthContext } from "./auth-context";
+import { signOut } from "@/server/signOut";
 
 export default function Header() {
+    const user = useContext(AuthContext);
+
     const handleLogout = () => {
         signOut();
         window.location.reload();
@@ -14,18 +17,20 @@ export default function Header() {
     return (
         <header className="flex items-center justify-between px-6 md:px-20 lg:px-32 py-5">
             <Logo />
-            <CheckAuth ifA={
-                <div className="flex items-center gap-2">
-                    <Button className="rounded h-9 px-4">Create</Button>
-                    <Button onClick={handleLogout} className="rounded h-9 w-9" size="icon" variant="outline"><LogOut className="h-3 w-3" /></Button>
-                </div>
-            }>
+            {!user.user ? (
                 <div className="flex items-center gap-2">
                     <Button className="rounded h-9 px-4" asChild>
                         <Link href="/signin">SignIn</Link>
                     </Button>
                 </div>
-            </CheckAuth>
+            ) : (
+                <div className="flex items-center gap-2">
+                    <Button className="rounded h-9 px-4" asChild>
+                        <Link href="/new">Create</Link>
+                    </Button>
+                    <Button onClick={handleLogout} className="rounded h-9 w-9" size="icon" variant="outline"><LogOut className="h-3 w-3" /></Button>
+                </div>
+            )}
         </header>
     )
 };
