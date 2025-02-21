ebbd0d77794409691ec476eb0b3dfd5d5bbc05c2
r2hu1 <hi@rahul.eu.org>
2025-02-21 13:18:08 +0530

    login / signup ui

---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <main>
+            <div className="grid gap-3">
+                <div className="flex items-center justify-between">
+                    <Logo />
+                    <Button title="back to homepage" variant="outline" size="icon" className="h-8 w-8" asChild>
+                        <Link href="/">
+                            <ChevronLeft className="h-4 w-4" />
+                        </Link>
+                    </Button>
+                </div>
+                <h1 className="text-xl font-medium mt-2">Nice to see you</h1>
+                <p className="text-base max-w-xs text-foreground/80 -mt-2.5">Create a anonymous account to continue using xvote.</p>
+            </div>
+            <div className="mt-10">
+                <form className="grid gap-2">
+                    <Label htmlFor="username">Username</Label>
+                    <Input autocomplete="off" id="username" name="username" placeholder="Anonymous-user-1" />
+                    <Label htmlFor="hash" className="mt-2">Hash</Label>
+                    <Input autocomplete="off" id="hash" name="hash" placeholder="*******" />
+                    <div>
+                        <Popover>
+                            <PopoverTrigger>
+                                <p className="text-sm text-foreground/80 hover:underline">What is hash?</p>
+                            </PopoverTrigger>
+                            <PopoverContent className="text-sm -mr-20">An 10 digit password created with your username</PopoverContent>
+                        </Popover>
+                    </div>
+                    <Button type="submit" className="mt-2">Sign Up</Button>
+                </form>
+            </div>
+        </main>
+    )
+}
