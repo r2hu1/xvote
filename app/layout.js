88a596a4fc67697857403792dce7cc4f64513fc6
r2hu1 <hi@rahul.eu.org>
2025-02-21 14:42:55 +0530

    sonner

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const font = Bricolage_Grotesque({
   subsets: ["latin"],
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={font.className}>
         {children}
+        <Toaster position="top-center" visibleToasts={1} />
       </body>
     </html>
   );
