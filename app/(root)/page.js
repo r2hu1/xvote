7f24b06e97ad9290f0a7de979967803914c19d8b
r2hu1 <hi@rahul.eu.org>
2025-02-22 15:35:16 +0530

    loader

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -7,13 +7,16 @@ import { likePoll } from "@/server/poll";
 import { toast } from "sonner";
 import Poll from "@/components/poll";
 import { useContext, useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
   const [polls, setPolls] = useState([]);
   const [loadingPolls, setLoadingPolls] = useState({});
   const user = useContext(AuthContext);
+  const [fetching, setFetching] = useState(false);
 
   const getPolls = async () => {
+    setFetching(true);
     try {
       const req = await getAllPolls();
       const data = JSON.parse(req);
@@ -23,6 +26,10 @@ export default function Home() {
     } catch (e) {
       console.log(e);
     }
+    finally {
+      setFetching(false);
+    }
+
   };
 
   const updatePollInState = (updatedPoll) => {
@@ -76,6 +83,38 @@ export default function Home() {
     getPolls();
   }, []);
 
+  if (fetching) {
+    return (
+      <main className="px-6 py-10 mb-10 md:px-20 lg:px-32">
+        <div className="grid md:grid-cols-2 gap-5">
+          {Array.from({ length: 6 }).map((_, i) => (
+            <div key={i} className="p-3 border border-border rounded">
+              <div className="grid gap-1">
+                <Skeleton className="h-5" />
+                <div className="flex mt-3 items-center gap-2">
+                  <Skeleton className="h-4 w-10" />
+                  <Skeleton className="h-4 w-10" />
+                  <Skeleton className="h-4 w-10" />
+                </div>
+              </div>
+              <div className="mt-8 grid gap-3">
+                {Array.from({ length: 4 }).map((_, i) => (
+                  <Skeleton key={i} className="h-10 w-full" />
+                ))}
+              </div>
+              <div className="flex items-center justify-between mt-4">
+                <div className="flex items-center gap-2">
+                  <Skeleton className="h-6 w-12 rounded-full" />
+                  <Skeleton className="h-6 w-12 rounded-full" />
+                </div>
+                <Skeleton className="h-6 w-12" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    )
+  }
   return (
     <main className="px-6 py-10 mb-10 md:px-20 lg:px-32">
       <div className="grid md:grid-cols-2 gap-5">
