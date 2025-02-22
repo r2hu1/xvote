5346a206f78ef014de43fcbf636c0f560766e9ea
r2hu1 <hi@rahul.eu.org>
2025-02-22 12:46:44 +0530

    in single file

--- server/getUser.js
+++
@@ -1,9 +0,0 @@
-"use server"
-import { cookies } from "next/headers";
-import { verifyJWT } from "@/lib/jwt";
-
-export async function getUser() {
-    const token = cookies().get("token")?.value;
-    if (!token) return null;
-    return verifyJWT(token);
-}
--- server/signOut.js
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { cookies } from "next/headers";
-
-export async function signOut() {
-    cookies().set("token", "", { expires: new Date(0), path: "/" });
-    return { success: true };
-}
