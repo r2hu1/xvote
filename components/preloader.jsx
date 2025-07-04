37a82ee24d54e19b64321e09ab2c9abe25c70168
r2hu1 <newasram86@gmail.com>
2025-07-04 14:34:31 +0530

    feat: logo preloader

--- components/preloader.jsx
+++ components/preloader.jsx
@@ -1,16 +1,22 @@
 "use client";
 import { Loader2 } from "lucide-react";
 import { useEffect } from "react";
+import Logo from "./logo";
 
 export default function Preloader() {
-    useEffect(() => {
-        setTimeout(() => {
-            document.getElementById("preloader").style.display = "none";
-        }, 2000);
-    }, [])
-    return (
-        <div id="preloader" className="flex bg-background items-center justify-center h-full w-full absolute z-50 top-0 left-0 right-0">
-            <Loader2 className="animate-spin h-5 w-5" />
-        </div>
-    )
+  useEffect(() => {
+    setTimeout(() => {
+      document.getElementById("preloader").style.display = "none";
+    }, 2000);
+  }, []);
+  return (
+    <div
+      id="preloader"
+      className="flex bg-background items-center justify-center h-full w-full absolute z-[99999] top-0 left-0 right-0"
+    >
+      <div>
+        <Logo className="animate-pulse" />
+      </div>
+    </div>
+  );
 }
