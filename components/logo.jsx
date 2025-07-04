7fa31aa54b51fa576b4e5e575b78519e0f677008
r2hu1 <newasram86@gmail.com>
2025-07-04 14:35:04 +0530

    feat: added dynamic classes

--- components/logo.jsx
+++ components/logo.jsx
@@ -1,12 +1,15 @@
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-export default function Logo() {
-    return (
-        <Link href="/">
-            <div className="flex items-center gap-2">
-                <h1 className="font-bold text-lg bg-primary rounded-md text-primary-foreground w-8 h-8 flex items-center justify-center">X</h1>
-                <h1 className="text-lg font-bold">Vote</h1>
-            </div>
-        </Link>
-    )
+export default function Logo({ className }) {
+  return (
+    <Link href="/">
+      <div className={cn("flex items-center gap-2", className)}>
+        <h1 className="font-bold text-lg bg-primary rounded-md text-primary-foreground w-8 h-8 flex items-center justify-center">
+          X
+        </h1>
+        <h1 className="text-lg font-bold">Vote</h1>
+      </div>
+    </Link>
+  );
 }
